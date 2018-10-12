7dc0b4f6c132c87a268649f50aa0ba67978e41ea
503-08 <a503-08@503-08ui-iMac.local>
2018-10-12 18:15:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a503_08/a1012mobileweb/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a503_08.a1012mobileweb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        //리다이렉트가 있는 경우
+        //크롬으로 가지 않고 현재 앱에서 이동하도록 설정
+        webView.setWebViewClient(new WebViewClient());
+
+        //자바스크립트 사용 설정
+        WebSettings set = webView.getSettings();
+        set.setJavaScriptEnabled(true);
+
+        //확대 축소 버튼을 추가
+        set.setBuiltInZoomControls(true);
+
+        webView.loadUrl("http://192.168.0.118:8080/mobileweb");
+    }
+}
